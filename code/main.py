e45b1fd8cbabf886fa49892f95de517661fd4de2
Kateřina Remešová <remk01@vse.cz>
2022-11-22 18:07:13 +0100

    def make_doprava, fuknční id

--- code/main.py
+++ code/main.py
@@ -30,16 +30,12 @@ def make_doprava(num):
     transport_type_list = ['vlak',
                            'kamion',
                            'nákladní vozidlo']
-    team_list = [fake.color_name() for x in range(4)]
-    #id_list = [x for x in range(x1,Num)]
-
 
     fake_doprava = [{'Transit ID':x+1000,
-                     'Date':fake.date_between(start_date='2022-01-01T00:00:00.000Z', end_date='2022-06-30T00:00:00.000Z'),
+                     'Date':fake.date_between(start_date='-1y', end_date='today'),
                      'Region':np.random.choice(region_list),
                      'Poslytovatelé dopravy':np.random.choice(company_list),
-                     'Doprava':np.random.choice(transport_type_list),
-                     'Team':np.random.choice(team_list)} for x in range(num)]
+                     'Doprava':np.random.choice(transport_type_list, p=[0.30, 0.40, 0.30])} for x in range(num)]
     return fake_doprava
 
 df = pd.DataFrame(make_doprava(num=500))
